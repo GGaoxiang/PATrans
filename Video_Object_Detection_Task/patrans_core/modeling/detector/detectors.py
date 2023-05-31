2ad21121ba0ac0010583a39a9aebd5b25a1d0252
GGaoxiang <37071251+GGaoxiang@users.noreply.github.com>
2023-05-31 14:51:35 +0800

    Add files via upload

---
+++ Video_Object_Detection_Task/patrans_core/modeling/detector/detectors.py
@@ -0,0 +1,19 @@
+# Copyright (c) SenseTime Research and its affiliates. All Rights Reserved.
+from .generalized_rcnn import GeneralizedRCNN
+from .generalized_rcnn_rdn import GeneralizedRCNNRDN
+from .generalized_rcnn_mega import GeneralizedRCNNMEGA
+from .generalized_rcnn_fgfa import GeneralizedRCNNFGFA
+from .generalized_rcnn_stft import GeneralizedRCNNSTFT
+from .generalized_rcnn_patrans import GeneralizedRCNNPATRANS
+
+_DETECTION_META_ARCHITECTURES = {"GeneralizedRCNN": GeneralizedRCNN,
+                                 "GeneralizedRCNNRDN": GeneralizedRCNNRDN,
+                                 "GeneralizedRCNNMEGA": GeneralizedRCNNMEGA,
+                                 "GeneralizedRCNNFGFA": GeneralizedRCNNFGFA,
+                                 "GeneralizedRCNNSTFT": GeneralizedRCNNSTFT,
+                                 "GeneralizedRCNNPATRANS": GeneralizedRCNNPATRANS}
+
+
+def build_detection_model(cfg):
+    meta_arch = _DETECTION_META_ARCHITECTURES[cfg.MODEL.META_ARCHITECTURE]
+    return meta_arch(cfg)
